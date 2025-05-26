98b9c891f6a605e9cfe69fb513083c4ff04914a4
DeveloperDolon <dolonr718@gmail.com>
2025-05-26 19:28:43 +0600

    inventory management task initialize

---
+++ inventory-management/index.js
@@ -0,0 +1,8 @@
+
+const initial_stock = {tons: 1, kilogram: 0, grams: 0, milligrams: 0};
+
+const update_stock = (stock, type) => {
+    console.log(stock);
+}
+
+update_stock(initial_stock, 'sell');
